59040c85088763bda54919c112b2fb07f8a4b1b6
Mark Bell <me@markashleybell.com>
2011-06-28 08:07:25 +0100

    Initial commit

---
+++ microwiki.Web/Controllers/WikiController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace microwiki.Web.Controllers
+{
+    public class WikiController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
