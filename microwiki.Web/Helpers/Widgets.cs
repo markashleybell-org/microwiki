55aef15f4255f81dfcbe85732558da6293eb5f28
Mark Bell <me@markashleybell.com>
2011-07-19 14:11:25 +0100

    Removed document title from schema, implemented breadcrumb nav, closes #4

---
+++ microwiki.Web/Helpers/Widgets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+
+namespace microwiki.Web.Helpers
+{
+    public static class Widgets
+    {
+        public static MvcHtmlString Breadcrumb(this string url)
+        {
+            var fragments = url.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var output = new List<string>();
+            var path = "";
+
+            foreach(var fragment in fragments)
+            {
+                path += "/" + fragment;
+                output.Add("<a href=\"" + path + "\">" + fragment + "</a>");
+            }
+
+            return new MvcHtmlString("<a href=\"/\">root</a>" + ((output.Count > 0) ? " / " + string.Join(" / ", output.ToArray()) : ""));
+        }
+    }
+}
--- microwiki.Web/Models/DocumentViewModel.cs
+++ microwiki.Web/Models/DocumentViewModel.cs
@@ -8,10 +8,9 @@ namespace microwiki.Web.Models
     public class DocumentViewModel 
     {
         public string Location { get; set; }
-        public string Title { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastEdited { get; set; }
         public string Body { get; set; }
-        public List<dynamic> Children { get; set; }
+        public List<DocumentViewModel> Children { get; set; }
     }
 }
