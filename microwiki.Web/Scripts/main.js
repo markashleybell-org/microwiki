4e497b2449b4683ea5210d66c3bb767356b2f76a
Mark Bell <me@markashleybell.com>
2012-01-18 11:06:49 +0000

    Editor now uses PageDown (http://code.google.com/p/pagedown/)

--- microwiki.Web/Scripts/main.js
+++ microwiki.Web/Scripts/main.js
@@ -1,5 +1,21 @@
 ﻿$(function () {
 
+    var converter = new Markdown.Converter();
+
+//    converter.hooks.chain("preConversion", function (text) {
+//        return text.replace(/\b(a\w*)/gi, "*$1*");
+//    });
+
+//    converter.hooks.chain("plainLinkText", function (url) {
+//        return "This is a link to " + url.replace(/^https?:\/\//, "");
+//    });
+
+    var help = function () { alert("Do you need help?"); }
+
+    var editor = new Markdown.Editor(converter);
+
+    editor.run();
+
     $('#delete-form').bind('submit', function () {
 
         return confirm('Are you sure you want to permanently delete this page?');
