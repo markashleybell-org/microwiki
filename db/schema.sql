aec04d7d6499f3f2d7a09a7328b0e4618f595c1d
Mark Bell <me@markashleybell.com>
2015-03-13 14:59:53 +0000

    Basic CRUD scaffold

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,31 @@
-﻿CREATE DATABASE [MicroWiki]
+﻿USE [master]
+GO
+
+IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = N'MicroWiki')
+BEGIN
+	DECLARE @kill varchar(8000) = '';
+	SELECT @kill = @kill + 'kill ' + CONVERT(varchar(5), spid) + ';'
+	FROM master..sysprocesses 
+	WHERE dbid = db_id('MicroWiki')
+	EXEC(@kill)
+	DROP DATABASE [MicroWiki]
+END
+GO
+
+CREATE DATABASE [MicroWiki]
 GO
 
 USE [MicroWiki]
 GO
 
 CREATE TABLE [Documents] (
-    [ID] nvarchar(128) NOT NULL, 
-    [ParentID] nvarchar(128) NOT NULL, 
-    [Location] nvarchar(256) NOT NULL, 
-    [Username] nvarchar(128) NOT NULL, 
+    [ID] nvarchar(64) NOT NULL, 
+    [ParentID] nvarchar(64) NOT NULL, 
+    [Title] nvarchar(128) NOT NULL, 
     [Body] nvarchar(max) NULL,
+    [Slug] nvarchar(256) NULL, 
+    [Location] nvarchar(256) NULL, 
+    [Username] nvarchar(128) NOT NULL, 
     [Created] datetime NOT NULL,
     [Updated] datetime NOT NULL
 )
@@ -27,14 +43,16 @@ ALTER TABLE [Documents] ADD CONSTRAINT [DF_Documents_Updated] DEFAULT (getdate()
 GO
 
 CREATE TABLE [DeletedDocuments] (
-    [ID] nvarchar(128) NOT NULL, 
-    [ParentID] nvarchar(128) NOT NULL, 
-    [Location] nvarchar(256) NOT NULL, 
-    [Username] nvarchar(128) NOT NULL, 
+	[ID] nvarchar(64) NOT NULL, 
+    [ParentID] nvarchar(64) NOT NULL, 
+    [Title] nvarchar(128) NOT NULL, 
     [Body] nvarchar(max) NULL,
-    [Created] datetime NOT NULL, 
-    [Updated] datetime NOT NULL, 
-    [Deleted] datetime NULL
+    [Slug] nvarchar(256) NULL, 
+    [Location] nvarchar(256) NULL, 
+    [Username] nvarchar(128) NOT NULL, 
+    [Created] datetime NOT NULL,
+    [Updated] datetime NOT NULL,
+    [Deleted] datetime NOT NULL
 )
 GO
 
@@ -49,8 +67,13 @@ GO
 ALTER TABLE [DeletedDocuments] ADD CONSTRAINT [DF_DeletedDocuments_Deleted] DEFAULT (getdate()) FOR [Deleted]
 GO
 
+DECLARE @RootID nvarchar(128) = LOWER(NEWID())
+
 INSERT INTO [Documents] 
-    ([ID], [ParentID], [Location], [Username], [Body])
+    ([ID], [ParentID], [Title], [Body], [Location], [Username])
 VALUES 
-    ('HOME', 'home', 'home', 'MicroWiki', '# Welcome to MicroWiki!');
+    (@RootID, @RootID, 'Welcome to MicroWiki!', 'You can [Edit](/wiki/update/' + @RootID + ') this page now, or [Add Child Pages](/wiki/create/' + @RootID + ').', '/', 'SYSTEM');
+GO
+
+SELECT * FROM [Documents]
 GO
---
+++ microwiki/Models/DocumentCreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace microwiki.Models
+{
+    public class DocumentCreateViewModel
+    {
+        [Required]
+        public string ParentID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        public string Slug { get; set; }
+    }
+}
