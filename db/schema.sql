8e89f8197f213eb59c0bce1aea81fffe06396b7c
Mark Bell <me@markashleybell.com>
2015-03-13 08:20:37 +0000

    Rebuild as MVC 5 app, basic scaffolding

---
+++ db/schema.sql
@@ -0,0 +1,56 @@
+﻿CREATE DATABASE [MicroWiki]
+GO
+
+USE [MicroWiki]
+GO
+
+CREATE TABLE [Documents] (
+    [ID] nvarchar(128) NOT NULL, 
+    [ParentID] nvarchar(128) NOT NULL, 
+    [Location] nvarchar(256) NOT NULL, 
+    [Username] nvarchar(128) NOT NULL, 
+    [Body] nvarchar(max) NULL,
+    [Created] datetime NOT NULL,
+    [Updated] datetime NOT NULL
+)
+GO
+
+ALTER TABLE [Documents] ADD CONSTRAINT [PK_Documents] PRIMARY KEY ([ID])
+GO
+ALTER TABLE [Documents] ADD CONSTRAINT [FK_Documents_Documents] FOREIGN KEY (ParentID) REFERENCES [Documents] (ID) 	
+GO
+ALTER TABLE [Documents] ADD CONSTRAINT [DF_Documents_ParentID] DEFAULT ('home') FOR [ParentID]
+GO
+ALTER TABLE [Documents] ADD CONSTRAINT [DF_Documents_Created] DEFAULT (getdate()) FOR [Created]
+GO
+ALTER TABLE [Documents] ADD CONSTRAINT [DF_Documents_Updated] DEFAULT (getdate()) FOR [Updated]
+GO
+
+CREATE TABLE [DeletedDocuments] (
+    [ID] nvarchar(128) NOT NULL, 
+    [ParentID] nvarchar(128) NOT NULL, 
+    [Location] nvarchar(256) NOT NULL, 
+    [Username] nvarchar(128) NOT NULL, 
+    [Body] nvarchar(max) NULL,
+    [Created] datetime NOT NULL, 
+    [Updated] datetime NOT NULL, 
+    [Deleted] datetime NULL
+)
+GO
+
+ALTER TABLE [DeletedDocuments] ADD CONSTRAINT [PK_DeletedDocuments] PRIMARY KEY ([ID])
+GO
+ALTER TABLE [DeletedDocuments] ADD CONSTRAINT [DF_DeletedDocuments_ParentID] DEFAULT ('home') FOR [ParentID]
+GO
+ALTER TABLE [DeletedDocuments] ADD CONSTRAINT [DF_DeletedDocuments_Created] DEFAULT (getdate()) FOR [Created]
+GO
+ALTER TABLE [DeletedDocuments] ADD CONSTRAINT [DF_DeletedDocuments_Updated] DEFAULT (getdate()) FOR [Updated]
+GO
+ALTER TABLE [DeletedDocuments] ADD CONSTRAINT [DF_DeletedDocuments_Deleted] DEFAULT (getdate()) FOR [Deleted]
+GO
+
+INSERT INTO [Documents] 
+    ([ID], [ParentID], [Location], [Username], [Body])
+VALUES 
+    ('HOME', 'home', 'home', 'MicroWiki', '# Welcome to MicroWiki!');
+GO
---
+++ microwiki/Models/Document.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace microwiki.Models
+{
+    public class Document
+    {
+        public string ID { get; set; }
+        public string ParentID { get; set; }
+        public string Location { get; set; }
+        public string Username { get; set; }
+        public string Body { get; set; }
+
+        public List<Document> Children { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public DateTime? Deleted { get; set; }
+    }
+}
