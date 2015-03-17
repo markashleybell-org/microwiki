6d78caec5ee89616563d5b1a54604aed51695f7f
Mark Bell <me@markashleybell.com>
2015-03-17 13:36:19 +0000

    Tweak whitespace

--- microwiki/Scripts/main.js
+++ microwiki/Scripts/main.js
@@ -4,21 +4,14 @@
         //    converter.hooks.chain("preConversion", function (text) {
         //        return text.replace(/\b(a\w*)/gi, "*$1*");
         //    });
-
         //    converter.hooks.chain("plainLinkText", function (url) {
         //        return "This is a link to " + url.replace(/^https?:\/\//, "");
         //    });
         var help = function () { alert("Do you need help?"); };
         var editor = new Markdown.Editor(converter);
         //editor.hooks.set("insertImageDialog", function (callback) {
-        //    alert("Please click okay to start scanning your brain...");
-        //    setTimeout(function () {
-        //        var prompt = "We have detected that you like cats. Do you want to insert an image of a cat?";
-        //        if (confirm(prompt))
-        //            callback("http://icanhascheezburger.files.wordpress.com/2007/06/schrodingers-lolcat1.jpg")
-        //        else
-        //            callback(null);
-        //    }, 2000);
+        //    // Open upload/browse dialog and return 
+        //    // callback(null); once an image has been selected
         //    return true; // tell the editor that we'll take care of getting the image url
         //});
         editor.run();
