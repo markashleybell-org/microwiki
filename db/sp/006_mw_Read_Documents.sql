1af68137231be08ac17f2930c2845b27265c72b0
Mark Bell <me@markashleybell.com>
2015-03-17 07:43:25 +0000

    Move all SQL into procedures

---
+++ db/sp/006_mw_Read_Documents.sql
@@ -0,0 +1,42 @@
+USE [MicroWiki]
+GO
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_NAME = 'mw_Read_Documents')
+    DROP PROCEDURE [dbo].[mw_Read_Documents]
+GO
+
+CREATE PROCEDURE [dbo].[mw_Read_Documents] 
+(
+    @ParentID nvarchar(64) = NULL
+)
+AS
+BEGIN 
+	SET NOCOUNT ON
+	
+	IF @ParentID IS NOT NULL
+	BEGIN
+	    SELECT 
+            ID,
+            ParentID,
+            Title,
+            Location
+        FROM 
+            Documents 
+        WHERE 
+            ID != @ParentID
+        AND
+            ParentID = @ParentID
+    END
+    ELSE
+    BEGIN
+        SELECT 
+            ID,
+            ParentID,
+            Title,
+            Location
+        FROM 
+            Documents
+    END
+END	
+GO
+
