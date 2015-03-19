2c0767c5bf74c7e6c27c8f88de667012e2f8835b
Mark Bell <me@markashleybell.com>
2015-03-19 07:33:38 +0000

    Commit rebuilt DB script

--- db/MicroWiki.sql
+++ db/MicroWiki.sql
@@ -344,6 +344,44 @@ GO
 USE [MicroWiki]
 GO
 
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_NAME = 'mw_Delete_Document')
+    DROP PROCEDURE [dbo].[mw_Delete_Document]
+GO
+
+CREATE PROCEDURE [dbo].[mw_Delete_Document] 
+(
+    @ID nvarchar(64),
+    @Username nvarchar(128)
+)
+AS
+BEGIN 
+	SET NOCOUNT ON
+	
+	INSERT INTO DeletedDocuments 
+	    (ID, ParentID, Title, Body, Slug, Location, Username, Created, Updated, Deleted)
+    SELECT
+        ID,
+        ParentID,
+        Title,
+        Body,
+        Slug,
+        Location,
+        @Username,
+        Created,
+        Updated,
+        GETDATE()
+    FROM
+        Documents 
+    WHERE
+        ID = @ID
+       
+    DELETE FROM Documents WHERE ID = @ID
+END	
+GO
+
+USE [MicroWiki]
+GO
+
 DECLARE @RootID nvarchar(128) = LOWER(NEWID())
 
 INSERT INTO [Documents] 
