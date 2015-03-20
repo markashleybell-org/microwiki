0cfd33c948e6accd763b76995ed0a064897abd36
Mark Bell <me@markashleybell.com>
2015-03-20 07:30:31 +0000

    Disable HandleErrorAttribute

--- microwiki/App_Start/FilterConfig.cs
+++ microwiki/App_Start/FilterConfig.cs
@@ -6,7 +6,7 @@ namespace microwiki
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            // filters.Add(new HandleErrorAttribute());
         }
     }
 }
