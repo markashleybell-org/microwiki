403e6b57f0d008888050269bd4026222084eafac
Mark Bell <me@markb.co.uk>
2015-03-24 13:56:41 +0000

    Commit build SQL scripts

--- db/MicroWiki.sql
+++ db/MicroWiki.sql
@@ -465,6 +465,31 @@ GO
 USE [MicroWiki]
 GO
 
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_NAME = 'mw_Check_File_Use')
+    DROP PROCEDURE [dbo].[mw_Check_File_Use]
+GO
+
+CREATE PROCEDURE [dbo].[mw_Check_File_Use] 
+(
+    @Location nvarchar(512)
+)
+AS
+BEGIN 
+	SET NOCOUNT ON
+
+	SELECT 
+        ID,
+        Title
+    FROM 
+        Documents 
+    WHERE
+        Body LIKE '%' + @Location + '%'
+END	
+GO
+
+USE [MicroWiki]
+GO
+
 DECLARE @RootID nvarchar(128) = LOWER(NEWID())
 
 INSERT INTO [Documents] 
--- db/MicroWiki_Procedures.sql
+++ db/MicroWiki_Procedures.sql
@@ -399,4 +399,29 @@ BEGIN
 END	
 GO
 
+USE [MicroWiki]
+GO
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_NAME = 'mw_Check_File_Use')
+    DROP PROCEDURE [dbo].[mw_Check_File_Use]
+GO
+
+CREATE PROCEDURE [dbo].[mw_Check_File_Use] 
+(
+    @Location nvarchar(512)
+)
+AS
+BEGIN 
+	SET NOCOUNT ON
+
+	SELECT 
+        ID,
+        Title
+    FROM 
+        Documents 
+    WHERE
+        Body LIKE '%' + @Location + '%'
+END	
+GO
+
 
