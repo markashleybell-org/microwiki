markashleybell-org/microwiki

commit fb250ad62a3a28bc1159b64e8e3242c69aa1fd06

added ispublic to wiki pages

db/Releases/004 Public Documents.sql
@@ -1,0 +1,324 @@
+﻿IF COL_LENGTH('[dbo].[Documents]', 'IsPublic') IS NULL
+BEGIN
+    ALTER TABLE [dbo].[Documents]
+	ADD [IsPublic] BIT NOT NULL DEFAULT 0
+END
+GO
+
+ALTER PROCEDURE [dbo].[CreateDocument]
+(
+    @ID UNIQUEIDENTIFIER,
+    @ParentID UNIQUEIDENTIFIER,
+    @Title NVARCHAR(128),
+    @Body NVARCHAR(max),
+    @Slug NVARCHAR(256),
+    @Username NVARCHAR(128),
+    @Tags [dbo].[TagList] READONLY,
+    @IsPublic bit
+)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    DECLARE @RandomSlug NVARCHAR(5)
+
+    WHILE EXISTS (SELECT * FROM Documents WHERE Slug = @Slug)
+    BEGIN
+        SET @RandomSlug = (SELECT LOWER(LEFT(NEWID(), 5)))
+        SET @Slug = @Slug + '-' + @RandomSlug
+    END
+
+    INSERT INTO
+        Documents (
+            ID,
+            ParentID,
+            Title,
+            Body,
+            Slug,
+            Username,
+            IsPublic
+        )
+    VALUES (
+        @ID,
+        @ParentID,
+        @Title,
+        @Body,
+        @Slug,
+        @Username,
+        @IsPublic
+    )
+
+    EXEC UpdateDocumentLocations
+
+    EXEC UpdateTags @ID, @Tags
+
+    SELECT Location FROM Documents WHERE ID = @ID
+END
+
+GO
+
+ALTER PROCEDURE [dbo].[ReadDocument]
+(
+    @ID UNIQUEIDENTIFIER = NULL,
+    @Location NVARCHAR(256) = NULL
+)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    DECLARE @Document TABLE (
+        [ID]       UNIQUEIDENTIFIER  NOT NULL,
+        [ParentID] UNIQUEIDENTIFIER   NULL,
+        [Title]    NVARCHAR (128) NOT NULL,
+        [Body]     NVARCHAR (MAX) NULL,
+        [Slug]     NVARCHAR (256) NULL,
+        [Location] NVARCHAR (256) NULL,
+        [Tags]     NVARCHAR(1024) NULL,
+        [Username] NVARCHAR (128) NOT NULL,
+        [Created]  DATETIME NOT NULL,
+        [Updated]  DATETIME NOT NULL,
+        [IsPublic] BIT NOT NULL
+    )
+
+    IF @ID IS NOT NULL
+    BEGIN
+        INSERT INTO
+            @Document
+        SELECT
+            ID,
+            ParentID,
+            Title,
+            Body,
+            Slug,
+            Location,
+            NULL,
+            Username,
+            Created,
+            Updated,
+            IsPublic
+        FROM
+            Documents
+        WHERE
+            ID = @ID
+    END
+    ELSE IF @Location IS NOT NULL
+    BEGIN
+        INSERT INTO
+            @Document
+        SELECT
+            ID,
+            ParentID,
+            Title,
+            Body,
+            Slug,
+            Location,
+            NULL,
+            Username,
+            Created,
+            Updated,
+            IsPublic
+        FROM
+            Documents
+        WHERE
+            Location = @Location
+    END
+
+    UPDATE
+        doc
+    SET
+        Tags = (
+            SELECT STUFF((
+                SELECT
+                    '|' + [Label]
+                FROM
+                    Tags
+                INNER JOIN
+                    Tags_Documents ON Tags_Documents.TagID = Tags.ID
+                WHERE
+                    Tags_Documents.DocumentID = doc.ID
+                FOR XML PATH ('')
+            ), 1, 1, '')
+        )
+    FROM
+        @Document doc
+
+    SELECT * FROM @Document
+END
+
+GO
+
+ALTER PROCEDURE [dbo].[ReadDocuments]
+(
+    @ParentID UNIQUEIDENTIFIER = NULL
+)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    IF @ParentID IS NOT NULL
+    BEGIN
+        SELECT
+            ID,
+            ParentID,
+            Title,
+            Location,
+            IsPublic
+        FROM
+            Documents
+        WHERE
+            ParentID = @ParentID
+        ORDER BY
+            Title
+    END
+    ELSE
+    BEGIN
+        SELECT
+            ID,
+            ParentID,
+            Title,
+            Location,
+            IsPublic
+        FROM
+            Documents
+        ORDER BY
+            Title
+    END
+END
+
+GO
+
+ALTER PROCEDURE [dbo].[SearchDocuments]
+(
+    @Query NVARCHAR(255),
+    @Tags [dbo].[TagList] READONLY
+)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    DECLARE @QueryResults [dbo].[GuidList]
+    DECLARE @TagResults [dbo].[GuidList]
+
+    DECLARE @SearchResults [dbo].[GuidList]
+
+    DECLARE @QuerySpecified BIT = CASE WHEN @Query IS NOT NULL THEN 1 ELSE 0 END
+    DECLARE @TagsSpecified BIT = CASE WHEN (SELECT COUNT(*) FROM @Tags) > 0 THEN 1 ELSE 0 END
+
+    IF @QuerySpecified = 1
+    BEGIN
+        INSERT INTO
+            @QueryResults
+        SELECT
+            d.ID
+        FROM
+            Documents d
+        WHERE
+            d.Title LIKE '%' + @Query + '%'
+        OR
+            d.Body LIKE '%' + @Query + '%'
+    END
+
+    IF @TagsSpecified = 1
+    BEGIN
+        INSERT INTO
+            @TagResults
+        SELECT
+            td.DocumentID
+        FROM
+            Tags_Documents td
+        INNER JOIN
+            Tags t ON t.ID = td.TagID
+        WHERE
+            t.[Label] IN (SELECT [Label] FROM @Tags)
+    END
+
+    IF @TagsSpecified = 0
+    BEGIN
+        -- Straight LIKE query
+        INSERT INTO @SearchResults
+        SELECT ID FROM @QueryResults
+    END
+    ELSE IF @QuerySpecified = 0
+    BEGIN
+        -- Tag-only query
+        INSERT INTO @SearchResults
+        SELECT ID FROM @TagResults
+    END
+    ELSE
+    BEGIN
+        -- Results must exist in *both* datasets
+        INSERT INTO @SearchResults
+        SELECT ID FROM @QueryResults
+        INTERSECT
+        SELECT ID FROM @TagResults
+    END
+
+    SELECT
+        d.ID,
+        d.Title,
+        d.Body,
+        d.Location,
+        Tags = (
+            SELECT STUFF((
+                SELECT
+                    '|' + [Label]
+                FROM
+                    Tags
+                INNER JOIN
+                    Tags_Documents ON Tags_Documents.TagID = Tags.ID
+                WHERE
+                    Tags_Documents.DocumentID = d.ID
+                FOR XML PATH ('')
+            ), 1, 1, '')
+        ),
+        d.IsPublic
+    FROM
+        Documents d
+    INNER JOIN
+        @SearchResults sr ON sr.ID = d.ID
+END
+
+GO
+
+ALTER PROCEDURE [dbo].[UpdateDocument]
+(
+    @ID UNIQUEIDENTIFIER,
+    @ParentID UNIQUEIDENTIFIER,
+    @Title NVARCHAR(128),
+    @Body NVARCHAR(max),
+    @Slug NVARCHAR(256),
+    @Username NVARCHAR(128),
+    @Tags [dbo].[TagList] READONLY,
+    @IsPublic BIT
+)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    DECLARE @RandomSlug NVARCHAR(5)
+
+    WHILE EXISTS (SELECT * FROM Documents WHERE Slug = @Slug AND ID != @ID)
+    BEGIN
+        SET @RandomSlug = (SELECT LOWER(LEFT(NEWID(), 5)))
+        SET @Slug = @Slug + '-' + @RandomSlug
+    END
+
+    UPDATE
+        Documents
+    SET
+        ParentID = @ParentID,
+        Title = @Title,
+        Body = @Body,
+        Slug = @Slug,
+        Username = @Username,
+        Updated = GETDATE(),
+        IsPublic = @IsPublic
+    WHERE
+        ID = @ID
+
+    EXEC UpdateDocumentLocations
+
+    EXEC UpdateTags @ID, @Tags
+END
+
+GO

db/Stored Procedures/CreateDocument.sql
@@ -7,7 +7,11 @@ CREATE PROCEDURE [dbo].[CreateDocument]
     @Body NVARCHAR(max),
     @Slug NVARCHAR(256),
     @Username NVARCHAR(128),
-    @Tags [dbo].[TagList] READONLY
+    @Tags [dbo].[TagList] READONLY,
+    @IsPublic [bit] NOT NULL
+
+
+
 )
 AS
 BEGIN
@@ -28,7 +32,8 @@ BEGIN
             Title,
             Body,
             Slug,
-            Username
+            Username,
+            IsPublic
         )
     VALUES (
         @ID,
@@ -36,7 +41,8 @@ BEGIN
         @Title,
         @Body,
         @Slug,
-        @Username
+        @Username,
+        @IsPublic
     )
 
     EXEC UpdateDocumentLocations

db/Scripts/seed.sql
@@ -15,9 +15,9 @@ GO
 DECLARE @RootID UNIQUEIDENTIFIER = NEWID()
 
 INSERT INTO [Documents]
-    ([ID], [ParentID], [Title], [Body], [Location], [Username])
+    ([ID], [ParentID], [Title], [Body], [Location], [Username],[IsPublic])
 VALUES
-    (@RootID, NULL, 'Welcome to MicroWiki!', 'You can [Edit](/wiki/update/' + CONVERT(NVARCHAR(64), @RootID) + ') this page now, or [Add a Child Page](/wiki/create?parentID=' + CONVERT(NVARCHAR(64), @RootID) + ').', '/', 'SYSTEM');
+    (@RootID, NULL, 'Welcome to MicroWiki!', 'You can [Edit](/wiki/update/' + CONVERT(NVARCHAR(64), @RootID) + ') this page now, or [Add a Child Page](/wiki/create?parentID=' + CONVERT(NVARCHAR(64), @RootID) + ').', '/', 'SYSTEM', 0);
 GO
 
 SELECT * FROM [Documents]

db/Stored Procedures/ReadDocument.sql
@@ -18,7 +18,8 @@ BEGIN
         [Tags]     NVARCHAR(1024) NULL,
         [Username] NVARCHAR (128) NOT NULL,
         [Created]  DATETIME NOT NULL,
-        [Updated]  DATETIME NOT NULL
+        [Updated]  DATETIME NOT NULL,
+        [IsPublic] BIT NOT NULL
     )
 
     IF @ID IS NOT NULL
@@ -35,7 +36,8 @@ BEGIN
             NULL,
             Username,
             Created,
-            Updated
+            Updated,
+            IsPublic
         FROM
             Documents
         WHERE
@@ -55,7 +57,8 @@ BEGIN
             NULL,
             Username,
             Created,
-            Updated
+            Updated,
+            IsPublic
         FROM
             Documents
         WHERE

db/Stored Procedures/ReadDocuments.sql
@@ -13,7 +13,8 @@ BEGIN
             ID,
             ParentID,
             Title,
-            Location
+            Location,
+            IsPublic
         FROM
             Documents
         WHERE
@@ -27,7 +28,8 @@ BEGIN
             ID,
             ParentID,
             Title,
-            Location
+            Location,
+            IsPublic
         FROM
             Documents
         ORDER BY

db/Stored Procedures/SearchDocuments.sql
@@ -82,7 +82,8 @@ BEGIN
                     Tags_Documents.DocumentID = d.ID
                 FOR XML PATH ('')
             ), 1, 1, '')
-        )
+        ),
+        d.IsPublic
     FROM
         Documents d
     INNER JOIN

db/Stored Procedures/UpdateDocument.sql
@@ -7,7 +7,8 @@ CREATE PROCEDURE [dbo].[UpdateDocument]
     @Body NVARCHAR(max),
     @Slug NVARCHAR(256),
     @Username NVARCHAR(128),
-    @Tags [dbo].[TagList] READONLY
+    @Tags [dbo].[TagList] READONLY,
+    @IsPublic BIT
 )
 AS
 BEGIN
@@ -29,6 +30,7 @@ BEGIN
         Body = @Body,
         Slug = @Slug,
         Username = @Username,
+        IsPublic = @IsPublic,
         Updated = GETDATE()
     WHERE
         ID = @ID

db/Tables/Documents.sql
@@ -8,6 +8,7 @@
     [Username] NVARCHAR (128) NOT NULL,
     [Created]  DATETIME       CONSTRAINT [DF_Documents_Created] DEFAULT (getdate()) NOT NULL,
     [Updated]  DATETIME       CONSTRAINT [DF_Documents_Updated] DEFAULT (getdate()) NOT NULL,
+    [IsPublic] BIT NOT NULL DEFAULT 0, 
     CONSTRAINT [PK_Documents] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_Documents_Documents] FOREIGN KEY ([ParentID]) REFERENCES [dbo].[Documents] ([ID])
 )

commit d89a1c0aed84e96dffd387734c8a5297f4d80326

CreateDocument procedure now uses correct parameter syntax
Issue introduced in fb250ad

db/Releases/004 Public Documents.sql
@@ -14,7 +14,7 @@ ALTER PROCEDURE [dbo].[CreateDocument]
     @Slug NVARCHAR(256),
     @Username NVARCHAR(128),
     @Tags [dbo].[TagList] READONLY,
-    @IsPublic bit
+    @IsPublic BIT
 )
 AS
 BEGIN

db/Stored Procedures/CreateDocument.sql
@@ -8,10 +8,7 @@ CREATE PROCEDURE [dbo].[CreateDocument]
     @Slug NVARCHAR(256),
     @Username NVARCHAR(128),
     @Tags [dbo].[TagList] READONLY,
-    @IsPublic [bit] NOT NULL
-
-
-
+    @IsPublic BIT
 )
 AS
 BEGIN