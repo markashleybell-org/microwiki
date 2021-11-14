2b5eede209c6b02a37e0951b00d3afc056ccd52c
Mark Bell <me@markb.co.uk>
2021-11-14 17:00:32 +0000

    Suppress tuple casing warning

--- microwiki/GlobalSuppressions.cs
+++ microwiki/GlobalSuppressions.cs
@@ -4,3 +4,8 @@ using System.Diagnostics.CodeAnalysis;
     category: "Design",
     checkId: "RCS1090: Add call to 'ConfigureAwait' (or vice versa).",
     Justification = "ASP.NET Core has no synchronisation context, and this isn't a library.")]
+
+[assembly: SuppressMessage(
+    category: "StyleCop.CSharp.NamingRules",
+    checkId: "SA1316: Tuple element names should use correct casing",
+    Justification = "No thanks, I hate it.")]
