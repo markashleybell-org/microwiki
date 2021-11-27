89313be90d42f129c6190bbe301ae962c1a3e7d0
Mark Bell <me@markb.co.uk>
2021-11-27 06:59:36 +0000

    Add release script for Lucene search

---
+++ db/Releases/006 Lucene Search.sql
@@ -0,0 +1,46 @@
+﻿DROP PROCEDURE [dbo].[SearchDocuments]
+GO
+
+CREATE PROCEDURE [dbo].[ReadDocumentsForSearchIndex]
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT
+        ID,
+        ParentID,
+        Title,
+        Body,
+        Slug,
+        Location,
+        CAST(NULL AS NVARCHAR(1024)) AS Tags,
+        Username,
+        Created,
+        Updated,
+        IsPublic
+    INTO
+        #AllDocuments
+    FROM
+        Documents
+
+    UPDATE
+        docs
+    SET
+        Tags = (
+            SELECT STUFF((
+                SELECT
+                    '|' + [Label]
+                FROM
+                    Tags
+                INNER JOIN
+                    Tags_Documents ON Tags_Documents.TagID = Tags.ID
+                WHERE
+                    Tags_Documents.DocumentID = docs.ID
+                FOR XML PATH ('')
+            ), 1, 1, '')
+        )
+    FROM
+        #AllDocuments docs
+
+    SELECT * FROM #AllDocuments
+END
